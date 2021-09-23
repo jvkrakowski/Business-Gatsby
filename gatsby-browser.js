e7aac10eab23afdb37ca0976382128c1f5818ee0
J.V Krakowski <krakowski.jv@gmail.com>
2021-09-23 09:47:45 -0400

    Implemented Push Notification
    As previously promised, push notifications were implemented to notify users of updates.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,13 @@
 import "./src/styles/grid.scss"
 import "./src/styles/typography.scss"
 import "./src/styles/elements.scss"
+
+export const onServiceWorkerUpdateReady = () => {
+    const answer = window.confirm(
+      `We've updated! ` +
+        `Do you want to reload and check it out?`
+    )
+    if (answer === true) {
+      window.location.reload()
+    }
+  }
